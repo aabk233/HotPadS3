ada406a9c5c03c586899590ca8362325200d25b4
MATEBOOKE-MAX\23827 <aabk9632@gmail.com>
2023-04-03 23:43:13 +0800

    蜂鸣器更新

--- lib/BUZZER/Buzzer.cpp
+++ lib/BUZZER/Buzzer.cpp
@@ -1,1 +1,19 @@
 #include"Buzzer.h"
+
+void Buzzer_Init()
+{
+    ledcSetup(BUZZER_CHANNEL,BUZZER_FREQ,BUZZER_RESOLUTION);//设置PWM通道、频率和占空比分辨率
+    ledcAttachPin(BUZZER_PIN,BUZZER_CHANNEL);//将PWM通道绑定到实际的GPIO    
+}
+
+void Buzzer_Voice(int duty)
+{
+    ledcWriteTone(BUZZER_CHANNEL,BUZZER_FREQ);
+    ledcWrite(BUZZER_CHANNEL,duty);
+}
+
+void Buzzer_Clear()
+{
+    ledcWriteTone(BUZZER_CHANNEL,BUZZER_FREQ);
+    ledcWrite(BUZZER_CHANNEL,0);
+}
--- lib/BUZZER/Buzzer.h
+++ lib/BUZZER/Buzzer.h
@@ -4,7 +4,14 @@
 #include"Arduino.h"
 
 #define BUZZER_PIN 3
+#define BUZZER_FREQ 2000//频率
+#define BUZZER_CHANNEL 0//通道
+#define BUZZER_RESOLUTION 8//位数 
 
+#define voice1 125
 
+void Buzzer_Init();
+void Buzzer_Voice(int duty);
+void Buzzer_Clear();
 
 #endif // !__BUZZER_H_
