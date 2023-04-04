44db34d897035f8b2a1244a07b0a8d2ffbd6a59f
MATEBOOKE-MAX\23827 <aabk9632@gmail.com>
2023-04-04 16:16:19 +0800

    write MAX6675 code

--- lib/MAX6675/Max6675.h
+++ lib/MAX6675/Max6675.h
@@ -7,4 +7,21 @@
 #define MAX6675_SO 19
 #define MAX6675_SCK 18
 
+class MAX6675{
+public:
+  MAX6675(int8_t SCLK, int8_t CS, int8_t MISO);
+
+  float readCelsius(void);
+  float readFahrenheit(void);
+
+  /*!    @brief  For compatibility with older versions
+         @returns Temperature in F or NAN on failure! */
+  float readFarenheit(void) { return readFahrenheit(); }
+
+private:
+  int8_t sclk, miso, cs;
+  uint8_t spiread(void);
+};
+
+
 #endif // !__MAX6675_H_
