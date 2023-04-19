8c3968601a566f8f07592f925175d00a0681cfa6
aabk <aaabk9632@outlook.com>
2023-04-19 15:47:18 +0800

    lcd ok

--- src/main.cpp
+++ src/main.cpp
@@ -115,13 +115,11 @@ void setup() {
   SSR_Init();
   // Buzzer_Init();
   LCD_Init();
-  tft.setTextSize(2);
-tft.setTextColor(TFT_GREEN);
-tft.setCursor(100, 100);
-
-tft.setTextDatum(MC_DATUM);
-
-tft.drawString("Moonbeam ", 10, 10);
+  tft.fillScreen(TFT_BLACK);                //屏幕颜色
+  tft.setCursor(50, 50, 1);                 //设置起始坐标(10, 10)，2 号字体
+  tft.setTextColor(TFT_YELLOW);             //设置文本颜色为白色
+  tft.setTextSize(2);                       //设置文字的大小 (1~7)
+  tft.print("work pls");
 }
 void loop() {
 }
